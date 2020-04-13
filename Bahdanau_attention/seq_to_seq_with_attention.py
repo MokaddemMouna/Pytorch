9d44f62cab9cd86c67c292bc461fd7c3da8788c9
MokaddemMouna <mouna.mokaddem@gmail.com>
2020-04-13 11:31:11 -0400

    completed the model

--- Bahdanau_attention/seq_to_seq_with_attention.py
+++ Bahdanau_attention/seq_to_seq_with_attention.py
@@ -169,3 +169,337 @@ class Decoder(nn.Module):
             return None  # start with zeros
 
         return torch.tanh(self.bridge(encoder_final))
+
+
+class BahdanauAttention(nn.Module):
+    """Implements Bahdanau (MLP) attention"""
+
+    def __init__(self, hidden_size, key_size=None, query_size=None):
+        super(BahdanauAttention, self).__init__()
+
+        # We assume a bi-directional encoder so key_size is 2*hidden_size
+        key_size = 2 * hidden_size if key_size is None else key_size
+        query_size = hidden_size if query_size is None else query_size
+
+        self.key_layer = nn.Linear(key_size, hidden_size, bias=False)
+        self.query_layer = nn.Linear(query_size, hidden_size, bias=False)
+        self.energy_layer = nn.Linear(hidden_size, 1, bias=False)
+
+        # to store attention scores
+        self.alphas = None
+
+    def forward(self, query=None, proj_key=None, value=None, mask=None):
+        assert mask is not None, "mask is required"
+
+        # We first project the query (the decoder state).
+        # The projected keys (the encoder states) were already pre-computated.
+        query = self.query_layer(query)
+
+        # Calculate scores.
+        scores = self.energy_layer(torch.tanh(query + proj_key))
+        scores = scores.squeeze(2).unsqueeze(1)
+
+        # Mask out invalid positions.
+        # The mask marks valid positions so we invert it using `mask & 0`.
+        scores.data.masked_fill_(mask == 0, -float('inf'))
+
+        # Turn scores to probabilities.
+        alphas = F.softmax(scores, dim=-1)
+        self.alphas = alphas
+
+        # The context vector is the weighted sum of the values.
+        context = torch.bmm(alphas, value)
+
+        # context shape: [B, 1, 2D], alphas shape: [B, 1, M]
+        return context, alphas
+
+def make_model(src_vocab, tgt_vocab, emb_size=256, hidden_size=512, num_layers=1, dropout=0.1):
+    "Helper: Construct a model from hyperparameters."
+
+    attention = BahdanauAttention(hidden_size)
+
+    model = EncoderDecoder(
+        Encoder(emb_size, hidden_size, num_layers=num_layers, dropout=dropout),
+        Decoder(emb_size, hidden_size, attention, num_layers=num_layers, dropout=dropout),
+        nn.Embedding(src_vocab, emb_size),
+        nn.Embedding(tgt_vocab, emb_size),
+        Generator(hidden_size, tgt_vocab))
+
+    return model.cuda() if USE_CUDA else model
+
+
+class Batch:
+    """Object for holding a batch of data with mask during training.
+    Input is a batch from a torch text iterator.
+    """
+
+    def __init__(self, src, trg, pad_index=0):
+
+        src, src_lengths = src
+
+        # the tokenization adds for each sample of src 3,2, at the end of the sample, which corresponds to the tokens </s> and <s>
+        self.src = src
+        self.src_lengths = src_lengths
+        self.src_mask = (src != pad_index).unsqueeze(-2)
+        self.nseqs = src.size(0)
+
+        self.trg = None
+        self.trg_y = None
+        self.trg_mask = None
+        self.trg_lengths = None
+        self.ntokens = None
+
+        if trg is not None:
+            trg, trg_lengths = trg
+
+            # the tokenization adds 2 at the start of the sentence and 3 at the end of the sentence
+            self.trg = trg[:, :-1]
+            self.trg_lengths = trg_lengths
+            self.trg_y = trg[:, 1:]
+            self.trg_mask = (self.trg_y != pad_index)
+            self.ntokens = (self.trg_y != pad_index).data.sum().item()
+
+        if USE_CUDA:
+            self.src = self.src.cuda()
+            self.src_mask = self.src_mask.cuda()
+
+            if trg is not None:
+                self.trg = self.trg.cuda()
+                self.trg_y = self.trg_y.cuda()
+                self.trg_mask = self.trg_mask.cuda()
+
+
+def run_epoch(data_iter, model, loss_compute, print_every=50):
+    """Standard Training and Logging Function"""
+
+    start = time.time()
+    total_tokens = 0
+    total_loss = 0
+    print_tokens = 0
+
+    for i, batch in enumerate(data_iter, 1):
+
+        out, _, pre_output = model.forward(batch.src, batch.trg,
+                                           batch.src_mask, batch.trg_mask,
+                                           batch.src_lengths, batch.trg_lengths)
+        loss = loss_compute(pre_output, batch.trg_y, batch.nseqs)
+        total_loss += loss
+        total_tokens += batch.ntokens
+        print_tokens += batch.ntokens
+
+        if model.training and i % print_every == 0:
+            elapsed = time.time() - start
+            print("Epoch Step: %d Loss: %f Tokens per Sec: %f" %
+                  (i, loss / batch.nseqs, print_tokens / elapsed))
+            start = time.time()
+            print_tokens = 0
+
+    return math.exp(total_loss / float(total_tokens))
+
+
+# For data loading.
+from torchtext import data, datasets
+
+if True:
+    import spacy
+
+    spacy_de = spacy.load('de')
+    spacy_en = spacy.load('en')
+
+
+    def tokenize_de(text):
+        return [tok.text for tok in spacy_de.tokenizer(text)]
+
+
+    def tokenize_en(text):
+        return [tok.text for tok in spacy_en.tokenizer(text)]
+
+
+    UNK_TOKEN = "<unk>"
+    PAD_TOKEN = "<pad>"
+    SOS_TOKEN = "<s>"
+    EOS_TOKEN = "</s>"
+    LOWER = True
+
+    # we include lengths to provide to the RNNs
+    SRC = data.Field(tokenize=tokenize_de,
+                     batch_first=True, lower=LOWER, include_lengths=True,
+                     unk_token=UNK_TOKEN, pad_token=PAD_TOKEN, init_token=None, eos_token=EOS_TOKEN)
+    TRG = data.Field(tokenize=tokenize_en,
+                     batch_first=True, lower=LOWER, include_lengths=True,
+                     unk_token=UNK_TOKEN, pad_token=PAD_TOKEN, init_token=SOS_TOKEN, eos_token=EOS_TOKEN)
+
+    MAX_LEN = 25  # NOTE: we filter out a lot of sentences for speed
+    train_data, valid_data, test_data = datasets.IWSLT.splits(
+        exts=('.de', '.en'), fields=(SRC, TRG),
+        filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and
+                              len(vars(x)['trg']) <= MAX_LEN)
+    MIN_FREQ = 5  # NOTE: we limit the vocabulary to frequent words for speed
+    SRC.build_vocab(train_data.src, min_freq=MIN_FREQ)
+    TRG.build_vocab(train_data.trg, min_freq=MIN_FREQ)
+
+    PAD_INDEX = TRG.vocab.stoi[PAD_TOKEN]
+
+
+train_iter = data.BucketIterator(train_data, batch_size=64, train=True,
+                                 sort_within_batch=True,
+                                 sort_key=lambda x: (len(x.src), len(x.trg)), repeat=False,
+                                 device=DEVICE)
+valid_iter = data.Iterator(valid_data, batch_size=1, train=False, sort=False, repeat=False,
+                           device=DEVICE)
+
+
+def rebatch(pad_idx, batch):
+    """Wrap torchtext batch into our own Batch class for pre-processing"""
+    b = Batch(batch.src, batch.trg, pad_idx)
+    return b
+
+
+def greedy_decode(model, src, src_mask, src_lengths, max_len=100, sos_index=1, eos_index=None):
+    """Greedily decode a sentence."""
+
+    with torch.no_grad():
+        encoder_hidden, encoder_final = model.encode(src, src_mask, src_lengths)
+        prev_y = torch.ones(1, 1).fill_(sos_index).type_as(src)
+        trg_mask = torch.ones_like(prev_y)
+
+    output = []
+    attention_scores = []
+    hidden = None
+
+    for i in range(max_len):
+        with torch.no_grad():
+            out, hidden, pre_output = model.decode(
+                encoder_hidden, encoder_final, src_mask,
+                prev_y, trg_mask, hidden)
+
+            # we predict from the pre-output layer, which is
+            # a combination of Decoder state, prev emb, and context
+            prob = model.generator(pre_output[:, -1])
+
+        _, next_word = torch.max(prob, dim=1)
+        next_word = next_word.data.item()
+        output.append(next_word)
+        prev_y = torch.ones(1, 1).type_as(src).fill_(next_word)
+        attention_scores.append(model.decoder.attention.alphas.cpu().numpy())
+
+    output = np.array(output)
+
+    # cut off everything starting from </s>
+    # (only when eos_index provided)
+    if eos_index is not None:
+        first_eos = np.where(output == eos_index)[0]
+        if len(first_eos) > 0:
+            output = output[:first_eos[0]]
+
+    return output, np.concatenate(attention_scores, axis=1)
+
+
+def lookup_words(x, vocab=None):
+    if vocab is not None:
+        x = [vocab.itos[i] for i in x]
+
+    return [str(t) for t in x]
+
+def print_examples(example_iter, model, n=2, max_len=100,
+                   sos_index=1,
+                   src_eos_index=None,
+                   trg_eos_index=None,
+                   src_vocab=None, trg_vocab=None):
+    """Prints N examples. Assumes batch size of 1."""
+
+    model.eval()
+    count = 0
+    print()
+
+    if src_vocab is not None and trg_vocab is not None:
+        src_eos_index = src_vocab.stoi[EOS_TOKEN]
+        trg_sos_index = trg_vocab.stoi[SOS_TOKEN]
+        trg_eos_index = trg_vocab.stoi[EOS_TOKEN]
+    else:
+        src_eos_index = None
+        trg_sos_index = 1
+        trg_eos_index = None
+
+    for i, batch in enumerate(example_iter):
+
+        src = batch.src.cpu().numpy()[0, :]
+        trg = batch.trg_y.cpu().numpy()[0, :]
+
+        # remove </s> (if it is there)
+        src = src[:-1] if src[-1] == src_eos_index else src
+        trg = trg[:-1] if trg[-1] == trg_eos_index else trg
+
+        result, _ = greedy_decode(
+            model, batch.src, batch.src_mask, batch.src_lengths,
+            max_len=max_len, sos_index=trg_sos_index, eos_index=trg_eos_index)
+        print("Example #%d" % (i + 1))
+        print("Src : ", " ".join(lookup_words(src, vocab=src_vocab)))
+        print("Trg : ", " ".join(lookup_words(trg, vocab=trg_vocab)))
+        print("Pred: ", " ".join(lookup_words(result, vocab=trg_vocab)))
+        print()
+
+        count += 1
+        if count == n:
+            break
+
+class SimpleLossCompute:
+    """A simple loss compute and train function."""
+
+    def __init__(self, generator, criterion, opt=None):
+        self.generator = generator
+        self.criterion = criterion
+        self.opt = opt
+
+    def __call__(self, x, y, norm):
+        x = self.generator(x)
+        loss = self.criterion(x.contiguous().view(-1, x.size(-1)),
+                              y.contiguous().view(-1))
+        loss = loss / norm
+
+        if self.opt is not None:
+            loss.backward()
+            self.opt.step()
+            self.opt.zero_grad()
+
+        return loss.data.item() * norm
+
+
+def train(model, num_epochs=10, lr=0.0003, print_every=100):
+    """Train a model on IWSLT"""
+
+    if USE_CUDA:
+        model.cuda()
+
+    # optionally add label smoothing; see the Annotated Transformer
+    criterion = nn.NLLLoss(reduction="sum", ignore_index=PAD_INDEX)
+    optim = torch.optim.Adam(model.parameters(), lr=lr)
+
+    dev_perplexities = []
+
+    for epoch in range(num_epochs):
+        print("Epoch", epoch)
+        model.train()
+        train_perplexity = run_epoch((rebatch(PAD_INDEX, b) for b in train_iter),
+                                     model,
+                                     SimpleLossCompute(model.generator, criterion, optim),
+                                     print_every=print_every)
+
+        model.eval()
+        with torch.no_grad():
+            print_examples((rebatch(PAD_INDEX, x) for x in valid_iter),
+                           model, n=3, src_vocab=SRC.vocab, trg_vocab=TRG.vocab)
+
+            dev_perplexity = run_epoch((rebatch(PAD_INDEX, b) for b in valid_iter),
+                                       model,
+                                       SimpleLossCompute(model.generator, criterion, None))
+            print("Validation perplexity: %f" % dev_perplexity)
+            dev_perplexities.append(dev_perplexity)
+
+    return dev_perplexities
+
+
+model = make_model(len(SRC.vocab), len(TRG.vocab),
+                   emb_size=256, hidden_size=256,
+                   num_layers=1, dropout=0.2)
+dev_perplexities = train(model, print_every=100)
